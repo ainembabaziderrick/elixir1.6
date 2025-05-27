8db61bc1a3d1b7b21b8875b85a4bddd1e8a2dc31
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-05-27 20:37:03 +0300

    functions start

--- times.ex
+++ times.ex
@@ -1,7 +1,21 @@
 defmodule Times do
 
-def double(n) do
-  n * 2
+# def double(n) do
+#   n * 2
+# end
+
+def double(n), do: n*2
+
+def greet(greeting, name), do: (
+IO.puts greeting
+IO.puts "How are you doing, #{name}!"
+)
+
+def triple(n) do
+  n*3
 end
 
+def quadruple(n) do
+  double(n) * 2
+end
 end
