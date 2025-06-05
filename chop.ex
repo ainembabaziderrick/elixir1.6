ed4f9d64b954bd60e925fb53da8765dcb725fff1
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-06-05 08:32:26 +0300

    chop

--- chop.ex
+++ chop.ex
@@ -1,21 +1,23 @@
 defmodule Chop do
+  require Logger
+
   def guess(actual, range), do: _guess(actual, range.first, range.last)
 
   def _guess(actual, low, high) when div(low + high, 2) == actual do
     guess = div(low + high, 2)
-    IO.puts("Is it #{guess}")
+    Logger.warning("Is it #{guess}")
     guess
   end
 
   def _guess(actual, low, high) when div(low + high, 2) > actual do
     guess = div(low + high, 2)
-    IO.puts("Is it #{guess}")
+    Logger.error("Is it #{guess}")
     _guess(actual, low, guess - 1)
   end
 
   def _guess(actual, low, high) when div(low + high, 2) < actual do
     guess = div(low + high, 2)
-    IO.puts("Is it #{guess}")
+    Logger.info("Is it #{guess}")
     _guess(actual, guess + 1, high)
   end
 end
