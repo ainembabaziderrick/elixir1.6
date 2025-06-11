a8a9fa3386e442fdd13a41e64a106e3d654746a3
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2025-06-11 08:35:46 +0300

    reduce

--- mylist.ex
+++ mylist.ex
@@ -12,4 +12,16 @@ defmodule MyList do
 
   def add_1([head | tail]), do: [head+1 | add_1(tail)]
 
+  def map([], _func), do: []
+
+  def map([head | tail], func), do: [func.(head) | map(tail,func)]
+
+  def reduce([], value, _) do
+    value
+  end
+
+  def reduce([head | tail], value, func) do
+    reduce(tail, func.(head, value), func)
+  end
+
 end
